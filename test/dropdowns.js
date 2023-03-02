b0be01758140cd1d8acaca75f960cfcaf4dc4fac
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2023-03-02 18:20:09 +0000

    add dummy object for if the test index gets merged

--- test/dropdowns.js
+++ test/dropdowns.js
@@ -37,7 +37,43 @@ let versionObject64 = {
         ],
     }
 }
-
+/*
+let versionObject64 = {
+    "Infdev": {
+        "Infdev 20100227 (Maxxx)": [
+            downloadSystem("v1","./mods/64bit/infdev/inf-20100227/minecraft-inf-20100227-v1-maxxx.zip"),
+        ],
+        "Infdev 20100313 (RVH)": [
+            // Todo: add notes system
+            {
+                version: "v0.0.1",
+                download: "./mods/64bit/infdev/inf-20100313/inf313_long_0.0.1.zip",
+            },
+            {
+                version: "v0.0.2",
+                download: "./mods/64bit/infdev/inf-20100313/inf313_long_0.0.2.zip",
+            },
+            {
+                version: "v0.0.3",
+                download: "./mods/64bit/infdev/inf-20100313/inf313_long_0.0.3.zip"
+            }
+        ],
+        "Infdev 20100325 (Maxxx)": [],
+        "Infdev 20100330 (Zero)": [],
+        "Infdev 20100414 (314rft)": [],
+        "Infdev 20100420 (314rft, Zero)": [],
+        "Infdev 20100624 (314rft)": [],
+    },
+    "Alpha": {
+        "Alpha v1.0.1_01 (Zeta and Zero)": [
+            {
+                version: "0.0.3_02",
+                download: "./mods/64bit/alpha/a1.0.1_01/minecraft-a1.0.1_01-64bit_v0.0.3_02-zero-and-zeta.zip",
+            }
+        ],
+    }
+}
+*/
 let downloads=document.getElementById("downloads64");
 let dropdown=document.getElementById("phase");
 function onchangeDropdown(value) {
