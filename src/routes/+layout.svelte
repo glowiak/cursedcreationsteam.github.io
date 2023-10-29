3403c4de937adbfd772b82b566dc523e1ee54047
code913 <54856929+code913@users.noreply.github.com>
2023-10-29 07:27:35 +0000

    feat: add material theming and index page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<slot />
+
+<style>
+    :global(body) {
+        background: var(--md-sys-color-background);
+        color: var(--md-sys-color-on-background);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Cursed Creations | Home</title>
+</svelte:head>
+
+<h1>hi</h1>
