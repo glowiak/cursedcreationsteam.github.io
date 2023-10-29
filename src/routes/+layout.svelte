c9cc30359bc3e568e08be4f1985d3d4856644404
code913 <54856929+code913@users.noreply.github.com>
2023-10-29 09:27:17 +0000

    ui: styles for main, p & code blocks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,20 @@
 <main><slot /></main>
 
 <style>
+    main {
+        padding: 2ch;
+    }
+
+    :global(p) {
+        max-inline-size: 75ch;
+    }
+
+    :global(code) {
+        background: var(--md-sys-color-surface-container-highest);
+        color: var(--md-sys-color-on-surface);
+        padding: 0 0.25ch;
+    }
+
     :global(body) {
         background: var(--md-sys-color-background);
         color: var(--md-sys-color-on-background);
