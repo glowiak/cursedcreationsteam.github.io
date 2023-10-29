ef979aa6976c4929853af5a9cbc15d4a0b692366
code913 <54856929+code913@users.noreply.github.com>
2023-10-29 17:16:25 +0000

    fix: loading a whole damn font for one icon

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -11,7 +11,8 @@
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <!-- svelte-ignore a11y-no-static-element-interactions -->
         <md-outlined-icon-button class="menu" aria-label="Toggle navigation" on:click={(_) => (navShown = !navShown)}>
-            <md-icon>menu</md-icon>
+            <!-- https://fonts.google.com/icons -->
+            <md-icon><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg></md-icon>
         </md-outlined-icon-button>
         <ul class="links" class:shown={navShown}>
             {#each [["/", "Home"], ["/downloads", "Mods"], ["/wiki", "Wiki"]] as [href, display]}
