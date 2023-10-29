b8929c7ec72079a0930f37d6a45f4f4ad9a4cc55
code913 <54856929+code913@users.noreply.github.com>
2023-10-29 15:33:55 +0000

    ui: move contact to page end

--- src/routes/downloads/+page.svelte
+++ src/routes/downloads/+page.svelte
@@ -1,11 +1,11 @@
 <h1>Cursed Creations Minecraft Mods</h1>
-<p>
-    For any support, contact the Cursed Creations team at <a
-        href="https://discord.gg/gBXj5j55uf">our Discord server</a
-    >
-</p>
 <ul>
     {#each [["/expandedworlds", "World Expansion (64bit and BigInteger Worldgen)"], ["/zetaforged", "ZetaForged (v1/v2)"], ["/fringelands_mod", "Allam A's Fringelands"]] as [href, display]}
         <li><a href="/downloads{href}">{display}</a></li>
     {/each}
 </ul>
+<p>
+    For any support, contact the Cursed Creations team at <a
+        href="https://discord.gg/gBXj5j55uf">our Discord server</a
+    >
+</p>
