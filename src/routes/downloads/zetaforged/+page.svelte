53c41b86d3aaa418985d2547afb299f3187e5bb3
code913 <54856929+code913@users.noreply.github.com>
2023-10-31 00:06:18 +0530

    ui: page title for zetaforged

--- src/routes/downloads/zetaforged/+page.svelte
+++ src/routes/downloads/zetaforged/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>Cursed Creations | ZetaForged</title>
+</svelte:head>
+
 <h1>ZetaForged</h1>
 <p>ZetaForged is a multi-functional utility mod for fabric 1.17 that does a lot of things, which include offsetting noise scale on individual X/Z axis as well as accessing a new dimension known as the Lush Nether.</p>
 <p>Version 1 of the mod is currently publicly available, with a planned v2 rewrite's development starting soon.</p>
